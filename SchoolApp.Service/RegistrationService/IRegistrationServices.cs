a79723786459b5b7fc4f06d657132b1813668fc1
ShivanshDots8081 <shivansh.sharma@dotsquares.com>
2022-09-12 10:38:22 +0530

    commit all changes

---
+++ SchoolApp.Service/RegistrationService/IRegistrationServices.cs
@@ -0,0 +1,22 @@
+﻿using SchoolApp.Data;
+using System;
+using System.Collections.Generic;
+
+namespace SchoolApp.Service.RegistrationService
+{
+  public  interface IRegistrationServices
+    {
+        bool GetUser(string userName, String password);
+        UserRegistration SavePresenter(UserRegistration user);
+        UserRegistration GetUserRegisterbyId(int Id);
+        UserRegistration UpdateuserRegister(UserRegistration userRegister);
+        bool isuserexist(string userName);
+        UserRegistration GetUserRegisterRole(string userName);
+        List<UserRegistration> GetAllUser(int Id);
+        string GetUserName(int Id);
+        int GetUserIdByEmail(string email);
+    }
+}
+
+    
+
---
+++ SchoolApp.Service/RegistrationService/RegistrationServices.cs
@@ -0,0 +1,79 @@
+﻿using SchoolApp.Data;
+using SchoolApp.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SchoolApp.Service.RegistrationService
+{
+    
+        public class RegistrationServices : IRegistrationServices
+    {
+
+            private readonly IRepository<UserRegistration> _repoUserRegistration;
+
+            public RegistrationServices(IRepository<UserRegistration> repoUserRegistration)
+            {
+            _repoUserRegistration = repoUserRegistration;
+            }
+
+            public bool GetUser(string email, string password)
+            {
+                var db = _repoUserRegistration.Query().Filter(x => x.EmailId == email && x.Password == password).Get().Any();
+                return db;
+            }
+
+            public bool isuserexist(string email)
+            {
+                var db = _repoUserRegistration.Query().Filter(x => x.EmailId == email).Get().Any();
+                return db;
+            }
+            public UserRegistration SavePresenter(UserRegistration user)
+            {
+            _repoUserRegistration.Insert(user);
+                return user;
+            }
+
+            public UserRegistration GetUserRegisterbyId(int Id)
+            {
+                return _repoUserRegistration.FindById(Id);
+            }
+
+            public UserRegistration UpdateuserRegister(UserRegistration userRegister)
+            {
+
+            _repoUserRegistration.Update(userRegister);
+                return userRegister;
+
+            }
+
+            public UserRegistration GetUserRegisterRole(string email)
+            {
+                return _repoUserRegistration.Query().Filter(x => x.EmailId == email).Get().FirstOrDefault();
+            }
+
+            public List<UserRegistration> GetAllUser(int Id)
+            {
+                return _repoUserRegistration.Query().Filter(x => x.UserId != Id).Get().ToList();
+            }
+
+            public string GetUserName(int Id)
+            {
+                return _repoUserRegistration.Query().Filter(x => x.UserId == Id).Get().FirstOrDefault().FirstName;
+            }
+
+            public List<UserRegistration> GetSenderName()
+            {
+                return _repoUserRegistration.Query().Get().ToList();
+            }
+            public int GetUserIdByEmail(string emailId)
+            {
+                return _repoUserRegistration.Query().Filter(x => x.EmailId == emailId).Get().FirstOrDefault().UserId;
+            }
+
+        }
+    }
+
+
+
